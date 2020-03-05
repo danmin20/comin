a0cab452de58aa9dcf4aaa3a9459d61a4ecffed4
Lee Jeong Min <danmin20@khu.ac.kr>
2020-03-05 21:18:31 +0900

    tab layout

--- app/src/main/java/com/danmin/comin/LectureActivity.kt
+++ app/src/main/java/com/danmin/comin/LectureActivity.kt
@@ -2,8 +2,12 @@ package com.danmin.comin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import com.danmin.comin.Fragment.FragmentAdapter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_lecture.*
+import kotlinx.android.synthetic.main.custom_tab.view.*
 
 class LectureActivity : AppCompatActivity() {
 
@@ -13,5 +17,36 @@ class LectureActivity : AppCompatActivity() {
 
         val fragmentAdapter = FragmentAdapter(supportFragmentManager)
         list_viewpager.adapter = fragmentAdapter
+
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("AI")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("CSS")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("HTML")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("ID")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("JPG")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("JS")))
+
+        list_viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                list_viewpager.currentItem = p0!!.position
+            }
+
+        })
+    }
+
+    private fun createTabView(tabName: String): View {
+        val tabView = LayoutInflater.from(baseContext).inflate(R.layout.custom_tab, null)
+
+        tabView.txt_name.text = tabName
+
+        return tabView
     }
 }
