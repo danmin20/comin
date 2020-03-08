9ef158affc80694d9df6088c1ec24464dccaf222
Lee Jeong Min <danmin20@khu.ac.kr>
2020-03-08 23:36:38 +0900

    implement social login

--- app/src/main/java/com/danmin/comin/Auth/MyPageActivity.kt
+++ app/src/main/java/com/danmin/comin/Auth/MyPageActivity.kt
@@ -1,7 +1,9 @@
 package com.danmin.comin.Auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.danmin.comin.MainActivity
 import com.danmin.comin.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -20,6 +22,13 @@ class MyPageActivity : AppCompatActivity() {
         val docRef = db.collection("users").document(auth.currentUser?.uid.toString())
         docRef.get().addOnSuccessListener { documentSnapshot ->
             nickname_area.setText(documentSnapshot.get("nickname").toString())
+        }
+
+        logout_button.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
 }
